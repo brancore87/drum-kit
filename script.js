599ca8a025088a78e52ab21befb59fa1fd37ef4c
Brandon N. Sangma <brancore87@gmail.com>
2024-03-04 16:03:01 +0530

    Added new sounds, logos and customized UIs

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ window.addEventListener("keydown", (e) => {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
   const key = document.querySelector(`.key[data-key="${e.key}"]`);
 
-  if (!audio) return; // stop the function from running all together
+  if (!audio) return; // stop the function from running altogether
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add("playing");
+});
+
+window.addEventListener("click", (e) => {
+  const clickedElement = e.target.closest(".key"); // Find the closest parent element with the class 'key'
+
+  if (!clickedElement) return; // If the clicked element is not a key, exit the function
+
+  const audioKey = clickedElement.dataset.key; // Access the data-key attribute of the clicked key
+  const audio = document.querySelector(`audio[data-key="${audioKey}"]`);
+  const key = document.querySelector(`.key[data-key="${audioKey}"]`);
+  console.log(audioKey);
+
+  if (!audio) return; // stop the function from running altogether
   audio.currentTime = 0; // rewind to the start
   audio.play();
   key.classList.add("playing");
